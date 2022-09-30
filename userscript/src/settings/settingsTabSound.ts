3034d9037ae6949011b449aa6bd715560941fdc9
AnnaBanana2577 <annabanana2577@outlook.com>
2022-09-30 14:26:48 -0400

    Added 'Enable Music Player' checkbox

--- userscript/src/settings/settingsTabSound.ts
+++ userscript/src/settings/settingsTabSound.ts
@@ -1,6 +1,8 @@
 import { saveSettings, SETTINGS } from "./settings";
+import { musicPlayer } from "../musicPlayer";
 
 export default function hookSoundSettingsTab() {
+  //Typewriter
   app.menu.settingsPanel.soundTab.addChild(
     (app.menu.settingsPanel.soundTab.typewriter = new Checkbox(
       "typewriter",
@@ -8,7 +10,9 @@ export default function hookSoundSettingsTab() {
       true
     ))
   );
-  app.menu.settingsPanel.soundTab.typewriter.x = app.menu.settingsPanel.soundTab.volumeSlider.x;
+
+  app.menu.settingsPanel.soundTab.typewriter.x =
+    app.menu.settingsPanel.soundTab.volumeSlider.x;
   app.menu.settingsPanel.soundTab.typewriter.y =
     app.menu.settingsPanel.soundTab.volumeSlider.y +
     app.menu.settingsPanel.soundTab.volumeSlider.height +
@@ -18,4 +22,25 @@ export default function hookSoundSettingsTab() {
     saveSettings();
   });
   app.menu.settingsPanel.soundTab.typewriter.setChecked(SETTINGS.typewriter);
+
+  //Music Player
+  app.menu.settingsPanel.soundTab.addChild(
+    (app.menu.settingsPanel.soundTab.musicPlayer = new Checkbox(
+      "musicplayer",
+      "Enable Music Player",
+      SETTINGS.enableMusicPlayer
+    ))
+  );
+
+  app.menu.settingsPanel.soundTab.musicPlayer.x =
+    app.menu.settingsPanel.soundTab.volumeSlider.x;
+  app.menu.settingsPanel.soundTab.musicPlayer.y =
+    app.menu.settingsPanel.soundTab.typewriter.y +
+    app.menu.settingsPanel.soundTab.typewriter.height +
+    14;
+  app.menu.settingsPanel.soundTab.musicPlayer.on(Checkbox.CHANGE, function (b) {
+    SETTINGS.enableMusicPlayer = b;
+    saveSettings();
+    if (!b) musicPlayer.stop(true);
+  });
 }
