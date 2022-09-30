069544bf38cc04042516dee578590e2906591e4f
AnnaBanana2577 <annabanana2577@outlook.com>
2022-09-30 11:07:45 -0400

    Added back and next functionality

--- userscript/src/musicPlayer.ts
+++ userscript/src/musicPlayer.ts
@@ -1,39 +1,43 @@
 const songs = [
   "https://www.dropbox.com/s/i7jc9ojpnsyii2l/Urban%20Sunrise%20.mp3?dl=1",
+  "https://www.dropbox.com/s/c26fdsksh9q0nzg/Images%20-%20Lost%20European.wav?dl=1",
+  "https://www.dropbox.com/s/36gyipvdty81o51/What%20a%20beautiful%20Sunset%21%20%28radio%20mix%29%20-%20Angelwing.wav?dl=1",
 ];
 
 export async function handleKeyDownMusicPlayer(e: KeyboardEvent) {
   if (e.repeat) return;
-  if (e.key == "F9") musicPlayer.start();
-  if (e.key == "F10") musicPlayer.stop();
+
+  switch (e.key) {
+    case "F6":
+      musicPlayer.back();
+      break;
+    case "F8":
+      musicPlayer.next();
+      break;
+    case "F7":
+      musicPlayer.isPlaying ? musicPlayer.stop() : musicPlayer.start();
+  }
 }
 
 class MusicPlayer {
+  public isPlaying = false;
   private currentSongIndex = 0;
   private songs: Array<Howl> = [];
 
   constructor(private songUrls: Array<string>) {
-    setTimeout(() => {
-      songUrls.forEach((song) =>
-        this.songs.push(
-          new Howl({
-            src: [song],
-            format: ["mp3"],
-            autoplay: false,
-            loop: true,
-            volume: 0.7,
-          })
-        )
-      );
-    }, 1000 * 7);
+    this.loadSongs();
   }
 
   public start() {
+    if (this.isPlaying) return;
     this.songs[this.currentSongIndex].play();
+    this.isPlaying = true;
   }
 
   public stop() {
+    if (!this.isPlaying) return;
     this.songs[this.currentSongIndex].stop();
+    this.isPlaying = false;
   }
 
   public next() {
@@ -51,6 +55,24 @@ class MusicPlayer {
     if (this.currentSongIndex-- < 0) this.currentSongIndex = 0;
 
     this.start();
+  }
+
+  private async loadSongs() {
+    setTimeout(() => {
+      //Wait 7 seconds before initalizing, to ensure Howler.js is loaded first
+      this.songUrls.forEach((song) =>
+        this.songs.push(
+          new Howl({
+            src: [song],
+            html5: true,
+            format: ["mp3"],
+            autoplay: false,
+            loop: true,
+            volume: 0.7,
+          })
+        )
+      );
+    }, 1000 * 7);
   }
 }
 
